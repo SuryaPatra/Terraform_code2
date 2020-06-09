f2b6f237525a3c25f786eb77d9bac0e180a9dc4a
SuryaPatra <40135093+SuryaPatra@users.noreply.github.com>
2020-06-09 19:02:31 +0530

    Updated AMI data source

--- main.tf
+++ main.tf
@@ -2,6 +2,26 @@ provider "aws" {
     region  = "us-east-1"
 }
 
+data "aws_ami" "ubuntu_ami" {
+    most_recent      = true
+    owners           = ["self"]
+
+    filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+}
+
+output "ami_id" {
+  value = data.aws_ami.ubuntu_ami.id
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-0b44050b2d893d5f7"
   instance_type = "t2.medium"
