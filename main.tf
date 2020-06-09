e4dc6103477917715472030e8fe7ab4b8d7b7332
SuryaPatra <40135093+SuryaPatra@users.noreply.github.com>
2020-06-09 18:51:09 +0530

    Updated availability zone

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0b44050b2d893d5f7"
   instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "HelloWorld"
