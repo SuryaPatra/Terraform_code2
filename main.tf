c1d4450638bb1971f8852b4a0ac07b0d129db177
SuryaPatra <40135093+SuryaPatra@users.noreply.github.com>
2020-06-18 13:15:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,10 +27,15 @@ output "ami_id" {
 
 resource "aws_instance" "web" {
   ami           = "ami-0b44050b2d893d5f7"
-  instance_type = "t2.micro"
-  availability_zone = "us-east-1g"
+  instance_type = "t2.medium"
+  availability_zone = "us-east-1a"
 
-  
+  tags = {
+    Name = "HelloWorld"
+    ttl  = "60"
+    Owner = "amazon"
+
+  }
 }
 
 # Security group
