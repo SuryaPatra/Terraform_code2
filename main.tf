66b74161e46f6b1f2d12c0d4275feed8d1e4fbaf
SuryaPatra <40135093+SuryaPatra@users.noreply.github.com>
2020-06-19 15:24:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,13 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami           = "ami-0b44050b2d893d5f7"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   availability_zone = "us-east-1a"
 
   tags = {
     Name = "HelloWorld"
-    owner = "amazon"
+    ttl  = "60"
+    Owner = "amazon"
 
   }
 }
