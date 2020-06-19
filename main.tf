a6a9c91791051851813bfdac3b8c6fb706aa2e97
SuryaPatra <40135093+SuryaPatra@users.noreply.github.com>
2020-06-19 14:22:45 +0530

    advisory policy

--- main.tf
+++ main.tf
@@ -3,61 +3,14 @@ provider "aws" {
     
 }
 
-data "aws_ami" "ubuntu_ami" {
-    most_recent      = true
-    owners           = ["amazon"]
-
-    filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  
-}
-
-output "ami_id" {
-  value = data.aws_ami.ubuntu_ami.id
-}
-
-
-
 resource "aws_instance" "web" {
   ami           = "ami-0b44050b2d893d5f7"
-  instance_type = "t2.medium"
+  instance_type = "t2.micro"
   availability_zone = "us-east-1a"
 
   tags = {
     Name = "HelloWorld"
-    ttl  = "60"
-    Owner = "amazon"
+    owner = "amazon"
 
-  }
-}
-
-# Security group
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  
-
-  tags = {
-    Name = "allow_tls"
-    ttl  = "60"
-    Owner = "amazon"
   }
 }
